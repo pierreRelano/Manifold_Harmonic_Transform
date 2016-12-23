691864e8e7a428a8fd200ee43ebb8a2c28869efc
pierreRelano <pierre.m@protonmail.com>
2016-12-23 22:37:53 +0100

    Clearer compute_manifold_laplacian function

--- compute_manifold_laplacian.m
+++ compute_manifold_laplacian.m
@@ -1,28 +1,19 @@
-function L = compute_manifold_laplacian(vertex, faces, type, options)
+function L = compute_manifold_laplacian(vertices, faces, type)
 %The combinatorial laplacian is a linear operator (thus a NxN matrix where N is the number of vertices). 
 %It depends only on the connectivity of the mesh, thus on face only.
 
-    if nargin<3
-        type = 'contan';
-    end
-
-    %TODO: retest the indice of combinatorial laplacian
+    %TODO: test the indice of combinatorial laplacian
     switch lower(type)
         case 'combinatorial'
-            %Compute edge list.
-            n = size(vertex,2);
-            E = [faces(:, [1 2]) faces(:, [2 3]) faces(:, [3 1])];
-            p = size(E,2);
-            %Compute the adjacency matrix.
-            W = sparse( E(:,1), E(:,1), ones(p,1) );
-            %symmetric
-            W = max(W,W');
-            %combinatorial Laplacian
-            D = spdiags(sum(W)', 0, n,n);
+            n = length(vertices(:,1));
+            %Compute Adjacency matrix
+            W = adjacency_matrix(faces);
+            %Degree matrix
+            D = full(spdiags(sum(W)', 0, n,n));
+            %symmetric combinatorial Laplacian
             L = D-W;
-
         case 'contan'
-        L = cotan_laplacian_matrix(vertex,faces) 
+        L = cotan_laplacian_matrix(vertices,faces); 
     end
 
 end
